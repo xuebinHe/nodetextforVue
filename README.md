74a08173b8ac1742d166314ca2237ec5360794bd
hexuebin@kingsoft.com <hexuebin@kingsoft.com>
2018-07-09 18:19:37 +0800

    marge 2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # nodetextforVue
+
+## test_base
+
+## test_compute_method_watch
+
+## test_calss&style
---
+++ test_class&style/index.js
@@ -0,0 +1,7 @@
+
+var app = new Vue ({
+    el: '#app',
+    data: {
+        message: '页面刷新时间：',
+    }
+});
