016952b4628d9136af59f2fa48fdfe1919d32758
hexuebin@kingsoft.com <hexuebin@kingsoft.com>
2018-07-10 11:00:53 +0800

    test for

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # nodetextforVue
 
 ## test_base
-
-## test_compute_method_watch
-
+vue2基本练习
+## test_computed_methods_watch
+vue2方法、计算属性、监听属性、部分生命周期钩子练习
 ## test_calss&style
-
+vue2 v-bind:class和v-bind:style练习
 ********
--- test_class&style/index.js
+++ test_class&style/index.js
@@ -2,6 +2,26 @@
 var app = new Vue ({
     el: '#app',
     data: {
-        message: '页面刷新时间：'
-    }
+        message: '页面刷新时间：',
+        time: new Date().toLocaleString(),
+        isRed: false,
+        isBlue: true,
+        fontsize: 1
+    },
+    methods: {
+        changeClass: function () {
+            this.isBlue = !this.isBlue;
+            this.isRed = !this.isRed;
+            this.fontsize += 1;
+            return ;
+        },
+    },
+    computed: {
+        comStyle: function () {
+            return {
+                redtext: this.isRed,
+                bluetext: this.isBlue
+            }
+        }
+    },
 });
